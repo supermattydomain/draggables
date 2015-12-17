f0f3770fcd59373b5e4dc4b726ec64bf68cef7f2
Matt <total_garbage_inovernivneriv8934@mailinator.com>
2015-12-17 22:15:45 +1100

    Set element matrix transforms via the SVGTransformable API instead of frobbing 'transform' attributes. Add and remove a 'selected' CSS class on elements being dragged.

--- svgdrag.js
+++ svgdrag.js
@@ -1,7 +1,6 @@
 var selectedElement = 0;
 var currentX = 0;
 var currentY = 0;
-var currentMatrix = 0;
 
 function onMouseMove(evt) {
 	moveElement(evt)
@@ -39,34 +38,81 @@ function onMouseClick(evt) {
 
 function selectElement(evt) {
 	selectedElement = evt.target;
+	// Recurse up the element tree until we find a <g> element,
+	// on which the display group's transforms are defined.
+	while (selectedElement.localName != 'g') {
+		selectedElement = selectedElement.parentElement;
+	}
+	// console.log("selected:", selectedElement, evt);
 	currentX = evt.clientX;
 	currentY = evt.clientY;
-	transform = selectedElement.getAttributeNS(null, "transform");
-	if (transform) {
-		currentMatrix = transform.slice(7, -1).split(' ');
-		for (var i = 0; i < currentMatrix.length; i++) {
-			currentMatrix[i] = parseFloat(currentMatrix[i]);
-		}
-	} else {
-		currentMatrix = [1, 0, 0, 1, 0, 0]; // Identity matrix
-		newMatrix = "matrix(" + currentMatrix.join(' ') + ")";
-		selectedElement.setAttributeNS(null, "transform", newMatrix);
-	}
-
 	selectedElement.setAttributeNS(null, "onmousemove", "onMouseMove(evt)");
 	selectedElement.setAttributeNS(null, "onmouseout", "onMouseOut(evt)");
 	selectedElement.setAttributeNS(null, "onmouseup", "onMouseUp(evt)");
 	selectedElement.setAttributeNS(null, "onmouseclick", "onMouseClick(evt)");
+	$(selectedElement).addClass('selected');
+}
+
+function transformToString(tr) {
+	var ret = '';
+	switch (tr.type) {
+	case SVGTransform.SVG_TRANSFORM_MATRIX:
+		// console.log('matrix', tr);
+		ret = ret + 'matrix(' + tr.matrix.a + ' ' + tr.matrix.b + ' ' + tr.matrix.c + ' ' + tr.matrix.d + ' ' + tr.matrix.e + ' ' + tr.matrix.f + ')'
+		break;
+	case SVGTransform.SVG_TRANSFORM_TRANSLATE:
+		// console.log('translate', tr);
+		ret = ret + 'translate(' + tr.matrix.e + ' ' + tr.matrix.f + ')'
+		break;
+	case SVGTransform.SVG_TRANSFORM_SCALE:
+		// console.log('scale', tr);
+		ret = ret + 'scale(' + tr.matrix.a + " " + tr.matrix.d + ')'
+		break;
+	case SVGTransform.SVG_TRANSFORM_ROTATE:
+		// console.log('rotate', tr);
+		ret = ret + 'rotate(' + Math.arcsin(tr.matrix.b) + ')'
+		break;
+	case SVGTransform.SVG_TRANSFORM_SKEWX:
+		// console.log('skewx', tr);
+		ret = ret + 'skewx(?)'
+		break;
+	case SVGTransform.SVG_TRANSFORM_SKEWY:
+		// console.log('skewy', tr);
+		ret = ret + 'skewy(?)'
+		break;
+	default:
+		throw "Unrecognised SVG node type " + tr.type;
+	}
+	return ret;
+}
+
+function dumpTransformList(name, l) {
+	var json_list = [];
+	for (var i = 0; i < l.numberOfItems; i++) {
+		json_list[i] = transformToString(l[i]);
+	}
+	console.log(name, json_list);
+}
+
+function dumpMatrix(name, m) {
+	console.log(name, [ m.a, m.b, m.c, m.d, m.e, m.f ])
 }
 
 function moveElement(evt) {
 	if (selectedElement !== 0) {
 		var dx = evt.clientX - currentX;
 		var dy = evt.clientY - currentY;
-		currentMatrix[4] += dx;
-		currentMatrix[5] += dy;
-		newMatrix = "matrix(" + currentMatrix.join(' ') + ")";
-		selectedElement.setAttributeNS(null, "transform", newMatrix);
+		var item, baseVal = selectedElement.transform.baseVal;
+		// dumpTransformList("Transform list before:", baseVal);
+		baseVal.consolidate();
+		var currentMatrix = baseVal.getItem(0).matrix;
+		// dumpMatrix('current:', currentMatrix);
+		currentMatrix.e += dx;
+		currentMatrix.f += dy;
+		baseVal.clear();
+		item = baseVal.createSVGTransformFromMatrix(currentMatrix);
+		baseVal.appendItem(item);
+		// dumpTransformList("Transform list after:", baseVal);
 		currentX = evt.clientX;
 		currentY = evt.clientY;
 	}
@@ -78,6 +124,7 @@ function deselectElement(evt) {
 		selectedElement.removeAttributeNS(null, "onmouseout");
 		selectedElement.removeAttributeNS(null, "onmouseup");
 		selectedElement.removeAttributeNS(null, "onmouseclick");
+		$(selectedElement).removeClass('selected');
 		selectedElement = 0;
 	}
 }
